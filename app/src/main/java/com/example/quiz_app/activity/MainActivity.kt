0058b8895877a68eda0c2757cc2b8b60f52aa041
Leonardo Armelin <leonardoarmelin@codeby.com.br>
2021-11-15 16:17:50 -0300

    chore(MainActivity): remove unused var

--- app/src/main/java/com/example/quiz_app/activity/MainActivity.kt
+++ app/src/main/java/com/example/quiz_app/activity/MainActivity.kt
@@ -2,13 +2,9 @@ package com.example.quiz_app.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.viewModels
 import com.example.quiz_app.R
-import com.example.quiz_app.viewModel.AppViewModel
 
 class MainActivity : AppCompatActivity() {
-
-    private val viewModel: AppViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
